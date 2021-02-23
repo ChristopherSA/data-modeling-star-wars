065a91800d804770c80f0420aa9793a9df6b345f
ChristopherSA <christopher.sevilla@outlook.com>
2021-02-23 18:14:21 +0000

    End

--- src/models.py
+++ src/models.py
@@ -8,26 +8,85 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class Planetas_props(Base):
+    __tablename__ = 'Planetas_props'
+    id = Column(Integer, primary_key=True)
+    planetas_id = Column(Integer, ForeignKey('Planetas.id'))
+    name = Column(String(250), nullable=False)
+    clima = Column(String(250))
+    diametro = Column(String(250))
+    gravedad = Column(String(250))
+    nombre = Column(String(250))
+    periodo_orbital = Column(String(250))
+    poblacion = Column(String(250))
+    habitantes = Column(String(250))
+    periodo_rotacion = Column(String(250))
+    superficie_acuatica = Column(String(250))
+    terreno = Column(String(250))
+    def to_dict(self):
+        return{}
+
+class Planetas(Base):
+    __tablename__ = 'Planetas'
+    id = Column(Integer, primary_key=True)
+    Nombre = Column(String(250))
+    Url = Column(String(250))
+    Planetas = relationship(Planetas_props)
+    def to_dict(self):
+        return{}
+
+
+class Personajes_props(Base):
+    __tablename__ = 'Personajes_props'
+    personaje_id = Column(Integer, ForeignKey('Personajes.id'))
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    altura = Column(String(250))
+    masa = Column(String(250))
+    pelo = Column(String(250))
+    piel = Column(String(250))
+    ojos = Column(String(250))
+    cumpleaños = Column(String(250))
+    genero = Column(String(250))
+    creacion = Column(String(250))
+    editado = Column(String(250))
+    nombre = Column(String(250))
+    mundo_origen = Column(String(250))
+    def to_dict(self):
+        return{}
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Personajes(Base):
+    __tablename__ = 'Personajes'
     id = Column(Integer, primary_key=True)
     street_name = Column(String(250))
     street_number = Column(String(250))
     post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
+    person_id = Column(Integer, ForeignKey('Personaje.id'))
+    Personaje = relationship(Personajes_props)
     def to_dict(self):
-        return {}
+        return{}
 
-## Draw from SQLAlchemy base
+class Favoritos(Base):
+    __tablename__ = 'Favoritos'
+    id = Column(Integer, primary_key=True)
+    usuario_id = Column(Integer, ForeignKey('Usuario.id'))
+    personajes_id = Column(Integer, ForeignKey('Personajes.id'))
+    planetas_id = Column(Integer, ForeignKey('Planetas.id'))
+    Personajes = relationship(Personajes_props)
+    Planetas = relationship(Planetas_props)
+    def to_dict(self):
+        return{}
+
+class Usuarios(Base):
+    __tablename__ = 'Usuario'
+    id = Column(Integer, primary_key=True)
+    usuario = Column(String(250), nullable=False)
+    apellido_1 = Column(String(250), nullable=False)
+    apellido_2 = Column(String(250), nullable=False)
+    id_usuario = Column(Integer, primary_key=True)
+    contraseña = Column(String(250), nullable=False)
+    Favoritos = relationship(Favoritos)
+    def to_dict(self):
+        return{}
+
 render_er(Base, 'diagram.png')
